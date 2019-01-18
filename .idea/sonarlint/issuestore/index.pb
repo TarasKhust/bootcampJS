d877d3a5c78b82d5a6d07adc892a7020a20dffd6
Taras Rushchak <taraskhust@gmail.com>
2019-01-18 16:49:05 +0200

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
+F
+notes-app/notes-app.js,6/b/6b5e8b63d99e64d038d8510b3881c515f10fcbc1
+G
+notes-app/notes-edit.js,3/7/3715a9f0bcfb3306718ca9775e44e100d0d07091
+L
+notes-app/notes-functions.js,7/c/7c3c5ee3b8163d7cf9522299d3653c931983efcc
+D
+todo-app/todo-app.js,e/6/e6a5ba4b7001e7cb37aad7f6f6c0004c97dd1ba8
+J
+todo-app/todo-functions.js,7/b/7b266b7fdb632159836183dd7f35892553032888
---
+++ oop/hangman.js
@@ -0,0 +1,26 @@
+const Hangman = function(word, remainingGuesses) {
+  this.word = word.toLowerCase().split(' ');
+  this.remainingGuesses = remainingGuesses;
+  this.quessedLetters = ['c'];
+};
+
+Hangman.prototype.getPuzzle = function() {
+  let puzzle = '';
+
+  this.word.forEach((letter) => {
+    if (this.quessedLetters.includes(letter)) {
+      puzzle += letter;
+    } else {
+      puzzle += '*';
+    }
+  });
+  return puzzle;
+};
+
+const game1 = new Hangman('Cat', 2);
+console.log(game1);
+
+game1.getPuzzle();
+/*?*/
+const game2 = new Hangman('New Jersey', 4);
+console.log(game2);
