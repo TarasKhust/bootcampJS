bd07e615448d56e47320be423ad037dfd3bef441
Taras Rushchak <taraskhust@gmail.com>
2019-01-29 16:34:23 +0200

    done

--- functions/promises.js
+++ functions/promises.js
@@ -1,52 +1,51 @@
 // Callback
 const getDataCallback = (num, callback) => {
-	setTimeout(() => {
-		if (typeof num === 'number') {
-			callback(undefined, num * 2);
-		} else {
-			callback('Number must be provided');
-		}
-	}, 2000);
-};
+    setTimeout(() => {
+        if (typeof num === 'number') {
+            callback(undefined, num * 2)
+        } else {
+            callback('Number must be provided')
+        }
+    }, 2000)
+}
 
 getDataCallback(2, (err, data) => {
-	if (err) {
-		console.log(err);
-	} else {
-		getDataCallback(data, (err, data) => {
-			if (err) {
-				console.log('err');
-			} else {
-				console.log(data);
-			}
-		});
-	}
-});
+    if (err) {
+        console.log(err)
+    } else {
+        getDataCallback(data, (err, data) => {
+            if (err) {
+                console.log('err')
+            } else {
+                console.log(data)
+            }
+        })
+    }
+})
 
 // Promise
 const getDataPromise = (num) => new Promise((resolve, reject) => {
-	setTimeout(() => {
-		typeof num === 'number' ? resolve(num * 2) : reject(
-				'Number must be provided');
-	}, 2000);
-});
+    setTimeout(() => {
+        typeof num === 'number' ? resolve(num * 2) : reject('Number must be provided')
+    }, 2000)
+})
 
 getDataPromise(2).then((data) => {
-	getDataPromise(data).then((data) => {
-		console.log(`Promise data: ${data}`);
-	}, (err) => {
-		console.log(err);
-	});
+    getDataPromise(data).then((data) => {
+        console.log(`Promise data: ${data}`)
+    }, (err) => {
+        console.log(err)
+    })
 }, (err) => {
-	console.log(err);
-});
+    console.log(err)
+})
 
-getDataPromise('10').then((data) => {
-	return getDataPromise(data);
+getDataPromise(10).then((data) => {
+    return getDataPromise(data)
 }).then((data) => {
-	return getDataPromise(data);
+    return 'this is some test data'
 }).then((data) => {
-	console.log(data);
+    console.log(data)
 }).catch((err) => {
-	console.log(err);
-});
+    console.log(err)
+})
